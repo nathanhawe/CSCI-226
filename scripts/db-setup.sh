faba77061488bd182ae22e331b1b8497390b1215
Nathan Hawe <nathanhawe@gmail.com>
2023-11-22 16:28:10 -0800

    Import EPA CRSI data.

--- scripts/db-setup.sh
+++ scripts/db-setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Run Migrations
-/opt/mssql-tools/bin/sqlcmd -H $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD -i $PROJECT_WD/migrations/0001_InitializeDatabase.sql
+for file in $PROJECT_WD/migrations/*.sql
+do
+    /opt/mssql-tools/bin/sqlcmd -H $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD -i $file >> $0
+done
+
+Import Data
+/opt/mssql-tools/bin/bcp CSCI226.dbo.EpaCrsi in $PROJECT_WD/data/2017_epa_crsi.dat -S $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD -F 2 -f $PROJECT_WD/data/EpaCrsi-c.fmt
